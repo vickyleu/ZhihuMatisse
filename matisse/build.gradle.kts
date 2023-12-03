35b804e15d817d5951697672c4bd6749a65582f1
vicky leu <yu6564172@gmail.com>
2023-12-03 16:09:37 +0800

    upgrade gradle script

--- matisse/build.gradle.kts
+++ matisse/build.gradle.kts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-plugins{
+plugins {
     id("com.android.library")
     kotlin("android")
     id("maven-publish")
@@ -22,6 +22,7 @@ plugins{
 
 group = "com.github.vickyleu"
 version = "1.0.0"
+
 
 android {
     compileSdk = 34
@@ -58,4 +59,23 @@ dependencies {
     implementation("it.sephiroth.android.library.imagezoom:imagezoom:2.3.0")
     implementation("io.fotoapparat:fotoapparat:2.7.0")
     compileOnly("com.github.bumptech.glide:glide:4.16.0")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            // Creates a Maven publication called "release".
+            register("release", MavenPublication::class) {
+                // Library Package Name (Example : "com.frogobox.androidfirstlib")
+                // NOTE : Different GroupId For Each Library / Module, So That Each Library Is Not Overwritten
+                groupId = "com.github.vickyleu"
+                // Library Name / Module Name (Example : "androidfirstlib")
+                // NOTE : Different ArtifactId For Each Library / Module, So That Each Library Is Not Overwritten
+                artifactId = "matisse"
+                // Version Library Name (Example : "1.0.0")
+                version = "1.0.0"
+                from(components["release"])
+            }
+        }
+    }
 }
